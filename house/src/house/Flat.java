6d86166a227d549fd09a6ca190f0909282a08e70
haran <haran@192.168.2.24>
2019-08-01 11:18:17 -0400

    Added a commment

---
+++ house/src/house/Flat.java
@@ -0,0 +1,16 @@
+package house;
+
+public class Flat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=9000000;
+		//int y=0;
+		for (int i=0; i<5; i++) {
+			x=16*5000000/100 + x;
+			System.out.println(x);
+		}
+		
+	}
+
+}
---
+++ house/src/house/TownHouse.java
@@ -0,0 +1,31 @@
+package house;
+
+public class TownHouse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 4000000;
+		int y = 0;
+		int z = 110000;
+		
+		
+		for(int i=0; i<43; i++) {
+			
+			
+			x=x+y;
+			y= x*10/1200 - z;
+			
+		 System.out.print( i);
+		 System.out.print('s');
+		 System.out.print(x);
+		 System.out.append("    ");
+		 
+		 System.out.println("Git repository");
+		}
+		
+		
+
+	}
+
+}
